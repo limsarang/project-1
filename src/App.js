2df79a9370daf4b206ffe0d9b06bf40db0ef9939
limsarang <w2014@e-mirim.hs.kr>
2022-07-13 04:49:29 +0900

    영화 리뷰 웹 서비스

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          첫 시작하는 리엑트 프로젝트!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          가보자고
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
